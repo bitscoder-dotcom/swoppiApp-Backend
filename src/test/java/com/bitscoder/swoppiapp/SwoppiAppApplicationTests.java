b73b62df42cf568b3004509e0c1152b7266eccbf
bitscoder <daniel.ijedibia@tm30.net>
2024-01-31 08:46:17 +0100

    first commit

---
+++ src/test/java/com/bitscoder/swoppiapp/SwoppiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitscoder.swoppiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwoppiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
